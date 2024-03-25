16de2f1b476b379470c1d48b619a0c1a6621940f
pnkwa <baconkwa89@gmail.com>
2024-03-25 15:46:04 +0700

    remove env

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,7 @@ import {
 } from 'react-router-dom'
 
 const appRoutes = createRoutesFromElements(
-  <Route
-    path={import.meta.env.BASE_URL}
-    element={<Outlet />}
-    errorElement={<div>not found</div>}
-  >
+  <Route element={<Outlet />} errorElement={<div>not found</div>}>
     <Route path="" element={<div className="text-red-300">bello eiei</div>} />
   </Route>
 )
--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,9 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
 import 'dotenv/config'
 
-/** replace the %PUBLIC_URL% */
-const cssPlugin = () => ({
-  name: 'css-transform',
-  transform(src, id) {
-    if (!/\.(css)$/.test(id)) return src
-    return src.replace(/%PUBLIC_URL%/g, process.env.PUBLIC_URL)
-  },
-})
-
 // eslint-disable-next-line import/no-unused-modules
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr(), cssPlugin()],
-  base: process.env.PUBLIC_URL ?? '/apps/bgc-dashboard',
-  server: { port: Number(process.env.PORT) || 3000 },
+  plugins: [react(), tsconfigPaths(), svgr()],
   optimizeDeps: { esbuildOptions: { target: 'esnext' } },
   build: { target: 'esnext' },
 })
